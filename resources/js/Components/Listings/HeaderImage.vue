6635370ec27380ddc9173b4f4a634577e48b5758
daverydan <danny.w.avery@gmail.com>
2024-05-01 18:27:02 -0400

    Fix save button in HeaderImage

--- resources/js/Components/Listings/HeaderImage.vue
+++ resources/js/Components/Listings/HeaderImage.vue
@@ -1,10 +1,12 @@
 <template>
     <div class="h-80 flex-1 bg-hero bg-cover bg-center bg-no-repeat cursor-pointer" :style="headerImageStyle" @click="$emit('header-clicked')">
         <div class="default-container relative h-80">
-            <ListingSave
-                :button="true"
-                class="absolute top-5 right-0"
-            />
+            <div class="w-full flex justify-end pt-5">
+                <ListingSave
+                    v-if="$page.props.auth.user"
+                    :button="true"
+                />
+            </div>
             <button type="button" class="absolute bottom-5 bg-white hover:bg-gray-100 text-sm text-black font-black rounded px-3 py-2 shadow transition duration-150">View Photos</button>
         </div>
     </div>
